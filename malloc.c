e68197cbe4f2d4780c270e3e8e48fc9bb25072c0
Harshwardhan Shukla <wardhanharsh091@gmail.com>
2026-03-20 23:10:54 +0530

    Add files via upload

---
+++ malloc.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<cs50.h>
+
+int main(void)
+{
+    char *s= get_string("s: ");
+    char *t = malloc(strlen(s)+1);
+
+    strcpy(t,s);
+
+    t[0]= toupper(t[0]);
+
+    printf("s: %s\n", s);
+    printf("t: %s\n", t);
+}
+
